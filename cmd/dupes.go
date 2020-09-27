8cdb05bbc0b674dc3a06312825c9ee2961c55654
Peter Wagner <git@mycloudand.me>
2020-09-27 08:07:11 -0400

    dupes output control - faster

--- cmd/dupes.go
+++ cmd/dupes.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"regexp"
 	"sort"
 	"strings"
 
@@ -17,14 +16,42 @@ var dupesCmd = &cobra.Command{
 	Short: "Find duplicate data",
 	RunE: runIndexRO(func(idx *archivist.Index, args []string) error {
 		out := os.Stdout
-		reportDupes(idx, out, archivist.FindTV, "tv")
-		reportDupes(idx, out, archivist.FindMovies, "movies")
+
+		// Parse arguments to filter output:
+		var tv, movies, details bool
+		var query []string
+		for _, a := range args {
+			if strings.Contains(a, "tv") {
+				tv = true
+			} else if strings.Contains(a, "movie") {
+				movies = true
+			} else if strings.Contains(a, "details") {
+				details = true
+			} else {
+				query = append(query, a)
+				details = true
+			}
+		}
+
+		queryRe, err := buildQueryRE(query)
+		if err != nil {
+			return err
+		}
+
+		both := !tv && !movies
+		if tv || both {
+			dupes := archivist.FindTV(idx, queryRe)
+			reportDupes(idx, out, details, dupes, "tv")
+		}
+		if movies || both {
+			dupes := archivist.FindMovies(idx, queryRe)
+			reportDupes(idx, out, details, dupes, "movies")
+		}
 		return nil
 	}),
 }
 
-func reportDupes(idx *archivist.Index, out io.Writer, lookup func(*archivist.Index, *regexp.Regexp) map[string][]string, mediaType string) {
-	dupes := lookup(idx, regexp.MustCompile("."))
+func reportDupes(idx *archivist.Index, out io.Writer, details bool, dupes map[string][]string, mediaType string) {
 	var names []string
 	for show, fses := range dupes {
 		if len(fses) > 1 {
@@ -36,10 +63,14 @@ func reportDupes(idx *archivist.Index, out io.Writer, lookup func(*archivist.Ind
 		_, _ = fmt.Fprintln(out, strings.Repeat("-", len(mediaType)+8))
 		sort.Strings(names)
 		for _, show := range names {
-			_, _ = fmt.Fprintf(out, "%s\n", show)
-			for _, fs := range dupes[show] {
-				fsSummary := archivist.Summarize(idx, fs, fmt.Sprintf("video/%s/%s", mediaType, show))
-				_, _ = fmt.Fprintf(out, "  %s - %s\n", fs, fsSummary)
+			if details {
+				_, _ = fmt.Fprintf(out, "%s\n", show)
+				for _, fs := range dupes[show] {
+					fsSummary := archivist.Summarize(idx, fs, fmt.Sprintf("video/%s/%s", mediaType, show))
+					_, _ = fmt.Fprintf(out, "  %s - %s\n", fs, fsSummary)
+				}
+			} else {
+				_, _ = fmt.Fprintf(out, "  %s || %d copies \n", show, len(dupes[show]))
 			}
 		}
 	}
---
+++ cmd/pull.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+const remoteName = "origin"
+
+var pullCmd = &cobra.Command{
+	Use:   "pull",
+	Short: "Pull upstream repo",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+
+		// Open repo:
+		indexPath := viper.GetString(flagIndex)
+		repo, err := git.PlainOpen(indexPath)
+		if err != nil {
+			return fmt.Errorf("opening repo: %w", err)
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"repo":   indexPath,
+			"remote": remoteName,
+		}).Info("fetching remote...")
+		if err := repoPull(ctx, repo); err != nil {
+			return err
+		}
+
+		h, err := repo.Head()
+		if err != nil {
+			return fmt.Errorf("checking repo HEAD: %w", err)
+		}
+		commit, err := repo.CommitObject(h.Hash())
+		if err != nil {
+			return fmt.Errorf("checking repo HEAD commit: %w", err)
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"commit": h.Hash(),
+			"date":   commit.Author.When.Format(time.RFC3339),
+		}).Info("updated")
+
+		return nil
+	},
+}
+
+func repoPull(ctx context.Context, repo *git.Repository) error {
+	wt, err := repo.Worktree()
+	if err != nil {
+		return fmt.Errorf("getting worktree: %w", err)
+	}
+
+	err = wt.PullContext(ctx, &git.PullOptions{
+		RemoteName: remoteName,
+		Progress:   os.Stdout,
+	})
+	if err != nil && err != git.NoErrAlreadyUpToDate {
+		return fmt.Errorf("pulling remote: %w", err)
+	}
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(pullCmd)
+}
