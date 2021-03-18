43997a3f6389df1702a9aa52aa44392dbd5ab1ae
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-03-18 12:31:03 -0400

    timestamppb.New

--- cmd/filesync.go
+++ cmd/filesync.go
@@ -7,11 +7,11 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/golang/protobuf/ptypes"
 	uuid "github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	archivist "github.com/thepwagner/archivist/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func SyncFilesystem(idx *archivist.Index, root string) error {
@@ -77,10 +77,7 @@ func SyncFilesystem(idx *archivist.Index, root string) error {
 			return fmt.Errorf("adding blob %q: %w", path, err)
 		}
 		log.WithField("blob_id", blob.Id).Debug("Indexed new path")
-		modTime, err := ptypes.TimestampProto(info.ModTime())
-		if err != nil {
-			return err
-		}
+		modTime := timestamppb.New(info.ModTime())
 
 		newPaths[pathRel] = &archivist.File{
 			BlobId:  blob.Id,
